36519a6a4103d977bf2a7feaaf158b41eeaa58cd
jod35 <jodestrevin@gmail.com>
2023-07-21 12:50:29 +0300

    document code

--- create_db.py
+++ create_db.py
@@ -3,6 +3,9 @@ import asyncio
 
 
 async def create_db():
+    """
+    coroutine responsible for creating database tables
+    """
     async with engine.begin() as conn:
         from models import Note
 
